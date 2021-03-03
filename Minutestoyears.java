f11a0a14714004a9a45efccd855665de40e2d222
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-03-03 11:26:06 +0530

    Create Minutestoyears.java
    this converts minutes to years

---
+++ Minutestoyears.java
@@ -0,0 +1,12 @@
+class MinutestoYears{
+int z = 210;
+public static void main(String[] args){
+int x = 3456789;
+int y =  6;
+MinutestoYears obj1 = new MinutestoYears();
+System.out.println(+x+  " minutes is approximately "  +y+  " years and "  +obj1.z+ " days");
+Years();
+}
+public static void Years(){
+}
+}
