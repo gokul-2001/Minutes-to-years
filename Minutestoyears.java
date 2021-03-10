46ee5d481e0f16e8a4c9a22ab179a7249442e70c
gokul-2001 <79854227+gokul-2001@users.noreply.github.com>
2021-03-10 20:05:19 +0530

    Update Minutestoyears.java
    Converts minutes to years and days

--- Minutestoyears.java
+++ Minutestoyears.java
@@ -1,12 +1,21 @@
-class MinutestoYears{
-int z = 210;
-public static void main(String[] args){
-int x = 3456789;
-int y =  6;
-MinutestoYears obj1 = new MinutestoYears();
-System.out.println(+x+  " minutes is approximately "  +y+  " years and "  +obj1.z+ " days");
-Years();
+import java.util.Scanner;
+public class MinutestoYears{
+
+    public static void main(String[] Strings) {
+
+
+        double minutesInYear = 60 * 24 * 365;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Input the number of minutes: ");
+
+        double min = input.nextDouble();
+
+        long years = (long) (min / minutesInYear);
+        int days = (int) (min / 60 / 24) % 365;
+
+        System.out.println((int) min + " minutes is approximately " + years + " years and " + days + " days");
+    }
 }
-public static void Years(){
-}
-}
+
